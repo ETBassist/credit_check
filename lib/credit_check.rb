da21c4a2a3f130782620a00305b2fcebddd99417
Eugene Theriault <hybridbassist@gmail.com>
2020-07-18 11:44:23 -0700

    Create credit card tests and solutions

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,7 +1,99 @@
 card_number = "5541808923795240"
 
 # Your Luhn Algorithm Here
+class Card
+  attr_accessor :number, :temp_array,  :valid
 
+  def initialize(number)
+    @number = number
+    @temp_array = []
+    @valid = false
+  end
+
+  def every_other_number
+    counter = 0
+    @number.split('').each do |digit|
+      if counter.even?
+       @temp_array << digit.to_i * 2
+      else
+       @temp_array << digit.to_i
+      end
+      counter += 1
+    end
+  end
+
+  def sum_double_digits
+    @temp_array = @temp_array.map do |number|
+      if number.digits.count > 1
+        number.digits[0] + number.digits[1]
+      else
+        number
+      end
+    end
+  end
+
+  def sum_all
+    @temp_array = @temp_array.sum
+    @valid = true if @temp_array % 10 == 0
+  end
+
+  def valid?
+    if @valid == true
+      "The number #{@number} is valid!"
+    else
+      "The number #{@number} is invalid!"
+    end
+  end
+
+end
 # Output
 ## If it is valid, print "The number [card number] is valid!"
 ## If it is invalid, print "The number [card number] is invalid!"
+# card.valid?
+# ===============================================================
+
+class CreditCard
+  attr_accessor :number, :card_limit, :valid, :temp_array
+
+  def initialize(number, card_limit)
+    @number = number
+    @card_limit = card_limit
+    @valid = false
+    @temp_array = []
+  end
+
+  def card_number
+    @number
+  end
+
+  def limit
+    @card_limit
+  end
+
+  def last_four
+    @number[-4..-1]
+  end
+
+  def is_valid?
+    counter = 0
+    @number.split('').each do |num|
+      if counter.even?
+        @temp_array << num.to_i * 2
+      else
+        @temp_array << num.to_i
+      end
+      counter += 1
+    end
+    @temp_array = @temp_array.map do |num|
+      if num.digits.count > 1
+        num.digits[0] + num.digits[1]
+      else
+        num
+      end
+    end
+    @temp_array = @temp_array.sum
+    @valid = true if @temp_array % 10 == 0
+    return @valid
+  end
+
+end
---
+++ test/credit_check_test.rb
@@ -0,0 +1,97 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/credit_check'
+
+class CreditCheckTest < Minitest::Test
+
+  def test_number_exists
+    card = Card.new("5541808923795240")
+
+    assert_equal "5541808923795240", card.number
+  end
+
+  def test_multiply_every_other
+    card = Card.new("5541808923795240")
+
+    card.every_other_number
+    assert_equal [10, 5, 8, 1, 16, 0, 16, 9, 4, 3, 14, 9, 10, 2, 8, 0], card.temp_array
+  end
+
+  def test_sum_of_double_digits
+    card = Card.new("5541808923795240")
+
+    card.every_other_number
+    card.sum_double_digits
+    assert_equal [1, 5, 8, 1, 7, 0, 7, 9, 4, 3, 5, 9, 1, 2, 8, 0], card.temp_array
+  end
+
+  def test_valid
+    card = Card.new("5541808923795240")
+    card.every_other_number
+    card.sum_double_digits
+    card.sum_all
+
+    assert card.valid
+  end
+
+  def test_full_run
+    card = Card.new("5541808923795240")
+    card.every_other_number
+    card.sum_double_digits
+    card.sum_all
+
+    assert_equal "The number 5541808923795240 is valid!", card.valid?
+  end
+
+  def test_invalid_number
+    card = Card.new("5541801923795240")
+    card.every_other_number
+    card.sum_double_digits
+    card.sum_all
+
+    assert_equal "The number 5541801923795240 is invalid!", card.valid?
+  end
+
+  def test_other_valid_number
+    card = Card.new("6011797668867828")
+    card.every_other_number
+    card.sum_double_digits
+    card.sum_all
+
+    assert_equal "The number 6011797668867828 is valid!", card.valid?
+
+  end
+
+
+  def test_credit_card_exists
+    card = CreditCard.new("6011797668867828", 1000)
+
+    assert_equal "6011797668867828", card.number
+    assert_equal 1000, card.limit
+  end
+
+  def test_last_four_digits
+    card = CreditCard.new("6011797668867828", 1000)
+
+    assert_equal "7828", card.last_four
+  end
+
+  def test_invalid_by_default
+    card = CreditCard.new("6011797668867828", 1000)
+
+    refute card.valid
+  end
+
+  def test_temp_array_is_array
+    card = CreditCard.new("6011797668867828", 1000)
+
+    assert card.temp_array.kind_of?(Array)
+  end
+
+  def test_full_function
+    card = CreditCard.new("6011797668867828", 1000)
+
+    assert card.is_valid?
+  end
+
+end
